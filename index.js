8c0229ed049d8cb5f3972d0bac1e14c81a2ad3c1
Jaakko Manninen <kschzt@gmail.com>
2019-04-07 19:50:06 +0300

    Pool arrays used for frame/MIDI events; multiple events per frame

--- index.js
+++ index.js
@@ -1,63 +1,87 @@
 import midi from 'midi'
 import startClock from './src/clock'
+import deepool from 'deepool'
 
-const channel = 0x8f + 14 // 0x8f + MIDI channel
-const velocity = 100
-const newNotes = [] // notes waiting to get on
-const noteOn = [ channel, 60, velocity ]
-const noteOff = [ channel, 60, 0 ]
-const noteOns = {}
-const noteOffs = {}
+const port = 4 // MIDI output port
+const channel = 0x8f + 1 // 0x8f + MIDI channel
+const frameEvents = new Map()
 let frameNumber = 0
 let patternStep = 0
 
-const triggerNote = (note, frameLength, framesFromNow = 0) => {
-  const frame = frameNumber + framesFromNow
-  noteOns[frame] = note
-  noteOffs[frame + frameLength] = note
+// Create array pool for frame MIDI events
+const pool = deepool.create(() => {
+  console.log('Grow Array Pool to', pool.size())
+  return [ 0, 0, 0 ]
+})
+pool.grow(8)
+
+// Trigger a note.
+const triggerNote = (channel, note, velocity, frameLength) => {
+  // Take arrays for on/off messages from the pool.
+  const noteOn = pool.use()
+  const noteOff = pool.use()
+  noteOn.length = noteOff.length = 0
+  noteOn[0] = noteOff[0] = channel
+  noteOn[1] = noteOff[1] = note
+  noteOn[2] = velocity
+  noteOff[2] = 0
+  scheduleFrameEvent(frameNumber, noteOn)
+  scheduleFrameEvent(frameNumber + frameLength, noteOff)
 }
 
-let notes = [ 20, 25, 32, 37, 44, 51, 58, 66, ]
-
-function triggerStep(step) {
-  console.log(step)
-  if (Math.random() > 0.3) {
-    triggerNote(60 + Math.random() * 80, 1)
+// Schedule a MIDI event for a frame.
+function scheduleFrameEvent(frameNumber, evt) {
+  let events = frameEvents.get(frameNumber)
+  if (!events) {
+    // Take an array for the frame's events from the pool.
+    events = pool.use()
+    events.length = 0
   }
+  events.push(evt)
+  frameEvents.set(frameNumber, events)
+}
 
+// Trigger the scheduled MIDI events for a frame.
+function triggerFrameEvents(frameNumber) {
+  const events = frameEvents.get(frameNumber)
+  for (let i = 0; i < events.length; i++) {
+    output.sendMessage(events[i])
+    // Return the event's array to the pool.
+    pool.recycle(events[i])
+  }
+  frameEvents.delete(frameNumber)
+  // Return the frame's array to the pool.
+  pool.recycle(events)
+}
+
+// Generate notes for pattern step.
+const notes = [ 24, 25, 36, 39, 48, 51, 60, ]
+function triggerStep(step) {
   if (step % 4 === 0) {
-    triggerNote(notes[Math.floor(Math.random() * notes.length)], 2)
+    triggerNote(channel, notes[Math.floor(Math.random() * notes.length)], 100, 4)
+  }
+  if (Math.random() > 0.3) {
+    triggerNote(channel, notes[Math.floor(Math.random() * notes.length)], 50, 2)
   }
 }
 
 // Connect MIDI
 const output = new midi.output()
-console.log(output.getPortName(0))
-output.openPort(0)
+console.log(output.getPortName(port))
+output.openPort(port)
 
 // Start the clock
 startClock(output, () => {
   frameNumber++
-
   patternStep = frameNumber / 6 - 1 % 16
 
-  // Trigger note-offs queued for this frame
-  if (noteOffs[frameNumber]) {
-    noteOff[1] = noteOffs[frameNumber]
-    output.sendMessage(noteOff)
-    delete noteOffs[frameNumber]
-  }
-
-  // Trigger logic for frame
+  // Trigger logic for step.
   if (frameNumber % 6 === 0) {
     triggerStep(patternStep % 16)
   }
 
-  // Trigger new notes created in this frame.
-  if (noteOns[frameNumber]) {
-    noteOn[1] = noteOns[frameNumber]
-    // Run note middleware here.
-    output.sendMessage(noteOn)
-    delete noteOns[frameNumber]
+  // Trigger MIDI events for this frame.
+  if (frameEvents.has(frameNumber)) {
+    triggerFrameEvents(frameNumber)
   }
 })
